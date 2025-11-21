53368fd82d8aedf9bfda1ecd9a11e943829d21ea
Dan Morris <dmorris@morrisdev.com>
2025-11-21 13:45:38 -0800

    fixed env link

--- src/app/services/ai.service.ts
+++ src/app/services/ai.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { ContactUs } from '../../interfaces/contactUs';
 import { Article } from '../../interfaces/article.interface';
 
